bc25efd33b3980136c80c903df0c86e525f51e3c
ansarimofid <ansarimofid@gmail.com>
2017-01-24 16:40:49 +0530

    Cleaned and commented js file

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@
 
 $(document).ready(function () {
 
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  // Smooth Scroll
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
           scrollTop: target.offset().top
@@ -21,51 +22,53 @@ $(document).ready(function () {
   var controller = new ScrollMagic.Controller();
 
 
+  // Intro Page animation
   var introPage = new ScrollMagic.Scene({
-    triggerElement:'#id_intro',
-    duration:'30%',
-    triggerHook:0
+    triggerElement: '#id_intro',
+    duration: '30%',
+    triggerHook: 0
   })
-    .setPin('#id_intro',{pushFollowers:false})
+    .setPin('#id_intro', {pushFollowers: false})
     .addTo(controller);
 
   var introPage2 = new ScrollMagic.Scene({
-    triggerElement:'#id_page1',
-    triggerHook:0.35
+    triggerElement: '#id_page1',
+    triggerHook: 0.35
   })
-    .setPin('#id_intro',{pushFollowers:false})
+    .setPin('#id_intro', {pushFollowers: false})
     .addTo(controller);
 
+  // Month Animation
   var scene1 = new ScrollMagic.Scene({
-    triggerElement:'#id_page1 h1',
-    duration:'50%',
-    triggerHook:0.75
+    triggerElement: '#id_page1 h1',
+    duration: '50%',
+    triggerHook: 0.75
   })
-    .setClassToggle('#id_page1','effect')
+    .setClassToggle('#id_page1', 'effect')
     .addTo(controller);
-  
+
   $('.month:not(:last-child)').each(function () {
     var monthScene = new ScrollMagic.Scene({
-      triggerElement:this,
-      duration:'67%',
-      triggerHook:-0.3
+      triggerElement: this,
+      duration: '67%',
+      triggerHook: -0.3
     })
-      .setPin(this,{pushFollowers:false})
+      .setPin(this, {pushFollowers: false})
       .addTo(controller);
   });
 
   $('.month').each(function () {
 
     var monthScene = new ScrollMagic.Scene({
-      triggerElement:this,
-      duration:'147%',
-      triggerHook:0.65
+      triggerElement: this,
+      duration: '147%',
+      triggerHook: 0.65
     })
-      .setClassToggle(this,'in-view-effect')
+      .setClassToggle(this, 'in-view-effect')
       .addTo(controller);
   });
 
-
+// Sidebar Reveal
   $('.nav-icon-wrapper').click(function (e) {
     var $this = $(this);
 
@@ -74,14 +77,15 @@ $(document).ready(function () {
     $this.parent().toggleClass('nav--show')
   });
 
+  // Share Overlay Reveal
   $('.share-btn').click(function (e) {
     e.preventDefault();
     $(this).closest('.main-wrapper').addClass('share-page--show');
   });
 
+  // Share Overlay Hide
   $('.share-page .close-icon').click(function (e) {
     e.preventDefault();
     $(this).closest('.main-wrapper').removeClass('share-page--show');
   })
-
 });
